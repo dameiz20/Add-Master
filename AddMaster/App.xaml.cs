60e6cc05342b51825f4aec5512faedac084582ec
Dakota Meisel <500162442@fvtc.edu>
2024-02-21 22:17:26 -0600

    Add project files.

---
+++ AddMaster/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using AddMaster.View;
+using Xamarin.Forms.Xaml;
+
+namespace AddMaster
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AddPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AddMaster/View/AddPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AddMaster.View
+{
+    public partial class AddPage : ContentPage
+    {
+        public AddPage()
+        {
+            InitializeComponent();
+        }
+
+        void Add_Clicked(System.Object sender, System.EventArgs e)
+        {
+            double dblFirstNum, dblSecondNum;
+
+            Double.TryParse(txtFirstNumber.Text, out dblFirstNum);
+            Double.TryParse(txtSecondNumber.Text, out dblSecondNum);
+            lblDisplay.Text = (dblFirstNum + dblSecondNum).ToString();
+
+        }
+        void Clear_Clicked(System.Object sender, System.EventArgs e)
+        {
+            lblDisplay.Text = "0";
+            txtFirstNumber.Text = string.Empty;
+            txtSecondNumber.Text = string.Empty; 
+        }
+    }
+}
